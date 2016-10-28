f07847905c22c678c5ea44341a86e0c7cfd58172
xjenek <clyde612@yandex.ru>
2016-10-28 10:33:13 +0300

    - x4 speed up - added CORS

--- index.js
+++ index.js
@@ -10,37 +10,37 @@ function Router() {
         POST: {},
         PUT: {},
         DELETE: {},
-        HEAD:{}
+        HEAD: {}
     };
 }
 
 Router.prototype.get = function (path, cb) {
     _path = genPath(path);
-    this.routes.GET[_path.path] = {params: _path.params, cb: cb};
+    this.routes.GET[_path.path] = {reg: new RegExp(_path.path, 'i'), params: _path.params, cb: cb};
     return this;
 };
 
 Router.prototype.post = function (path, cb) {
     _path = genPath(path);
-    this.routes.POST[_path.path] = {params: _path.params, cb: cb};
+    this.routes.POST[_path.path] = {reg: new RegExp(_path.path, 'i'), params: _path.params, cb: cb};
     return this;
 };
 
 Router.prototype.put = function (path, cb) {
     _path = genPath(path);
-    this.routes.PUT[_path.path] = {params: _path.params, cb: cb};
+    this.routes.PUT[_path.path] = {reg: new RegExp(_path.path, 'i'), params: _path.params, cb: cb};
     return this;
 };
 
 Router.prototype.delete = function (path, cb) {
     _path = genPath(path);
-    this.routes.DELETE[_path.path] = {params: _path.params, cb: cb};
+    this.routes.DELETE[_path.path] = {reg: new RegExp(_path.path, 'i'), params: _path.params, cb: cb};
     return this;
 };
 
 Router.prototype.head = function (path, cb) {
     _path = genPath(path);
-    this.routes.HEAD[_path.path] = {params: _path.params, cb: cb};
+    this.routes.HEAD[_path.path] = {reg: new RegExp(_path.path, 'i'), params: _path.params, cb: cb};
     return this;
 };
 
@@ -48,28 +48,17 @@ Router.prototype.other = function (method, path, cb) {
     _path = genPath(path);
     method = method.toUpperCase();
     if (!this.routes[method]) this.routes[method] = {};
-    this.routes[method][_path.path] = {params: _path.params, cb: cb};
+    this.routes[method][_path.path] = {reg: new RegExp(_path.path, 'i'), params: _path.params, cb: cb};
     return this;
 };
 
-Router.prototype.R = function () {
-    let routes = this.routes;
-    return function *(next) {
-        let r = chPath(routes, this.method, this.path);
-        if (r) {
-            if(this.method=='HEAD') this.body = '';
-            this.params = r.params;
-            yield r.cb;
-            return yield next;
-        } else {
-            this.body = 'Not found';
-            this.status = 404;
-            return this;
-        }
-    }
-};
 
 Router.prototype.add = function (path, routes) {
+    let fPath;
+    if (!routes && path && typeof path == 'object') {
+        routes = path;
+        path = '';
+    }
     if (path[path.length - 1] == '/') path = path.substr(0, path.length - 1);
     path = '^' + path.replace(/\//g, '\\/');
     routes = routes.routes;
@@ -78,9 +67,29 @@ Router.prototype.add = function (path, routes) {
             if (!this.routes[method]) this.routes[method] = {};
             for (let _path in routes[method]) {
                 if (routes[method].hasOwnProperty(_path)) {
-                    this.routes[method][path + (_path.substr(1))] = routes[method][_path];
+                    fPath = path + (_path.substr(1));
+                    this.routes[method][fPath] = routes[method][_path];
+                    this.routes[method][fPath].reg = new RegExp(fPath, 'i');
                 }
             }
+        }
+    }
+};
+
+Router.prototype.R = function (params = {}) {
+    let routes = this.routes;
+    return function *(next) {
+        let r = chPath(routes, this.method, this.path);
+        yield routeParams(params);
+        if (r) {
+            if (this.method == 'HEAD') this.body = '';
+            this.params = r.params;
+            yield r.cb;
+            yield next;
+        } else {
+            this.body = 'Not found';
+            this.status = 404;
+            yield next;
         }
     }
 };
@@ -103,7 +112,7 @@ function chPath(routes, method, path) {
     let params = {}, r, l;
     for (let k in routes[method]) {
         if (routes[method].hasOwnProperty(k)) {
-            r = path.match(new RegExp(k, 'i'));
+            r = path.match(routes[method][k].reg);
             if (r) {
                 l = Object.keys(routes[method][k].params).length;
                 for (let a = 0; a < l; a++) {
@@ -116,4 +125,15 @@ function chPath(routes, method, path) {
         }
     }
     return false;
+}
+
+function routeParams(params) {
+    return function *() {
+        if (params.cors) {
+            this.cors = {};
+            this.set('Access-Control-Allow-Origin', params.cors.Origin ? params.cors.Origin : '*');
+            this.set('Access-Control-Allow-Methods', params.cors.Methods ? params.cors.Methods : 'GET, POST, PUT, DELETE, HEAD');
+            this.set('Access-Control-Allow-Headers', params.cors.Headers ? params.cors.Headers : 'Origin, X-Requested-With, Content-Type, Accept');
+        }
+    };
 }
